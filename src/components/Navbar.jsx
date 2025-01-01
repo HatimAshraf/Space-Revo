6223947a9a562ba56520ab880609ce9d1b85c615
Hatim bin Ashraf <hatim.ashraf91@gmail.com>
2025-01-01 13:03:10 +0100

    Navbar UI completed

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,6 +1,7 @@
 import { useState } from 'react';
 import logo from '../assets/logo.png';
 import { RiCloseLine, RiMenu3Line } from '@remixicon/react';
+import { LINKS } from '../constants';
 
 function Navbar() {
   const [isOpen, setIsOpen] = useState(false);
@@ -26,6 +27,32 @@ function Navbar() {
             {isOpen ? <RiCloseLine /> : <RiMenu3Line />}
           </button>
         </div>
+        <div className='hidden md:flex space-x-8 md:space-x-4 pr-2'>
+          {LINKS.map((link, index) => (
+            <a
+              key={index}
+              href={link.link}
+              className='uppercase text-sm font-medium'
+            >
+              {link.name}
+            </a>
+          ))}
+        </div>
+      </div>
+      <div
+        className={`${
+          isOpen ? 'block' : 'hidden'
+        } md:hidden absolute bg-neutral-50 flex flex-col justify-center items-center w-full py-5 px-4 mt-2 border-b-4`}
+      >
+        {LINKS.map((link, index) => (
+          <a
+            href={link.link}
+            key={index}
+            className='uppercase text-lg font-medium block py-2 tracking-wide'
+          >
+            {link.name}
+          </a>
+        ))}
       </div>
     </nav>
   );
